8ecd615c1d1103d504e8f928e5e81b3ae613c8dc
Gaara <sabakugaara@users.noreply.github.com>
2018-02-13 19:08:12 +0800

    feat: add new option validate
    default is `true`, if false, will not validate specfication

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const {resolveRefs: resolve} = require('json-refs')
 const parse = require('./lib/parse')
 const validate = require('./lib/validate')
 
-module.exports = function(root, pattern = '**/*.spec.yaml', suffix = '.spec.yaml', prefix = 'api', shadow = false) {
+module.exports = function(root, pattern = '**/*.spec.yaml', suffix = '.spec.yaml', prefix = 'api', shadow = false, validate = true) {
   const definition = parse(root, pattern, suffix, prefix, shadow)
 
   return resolve(definition).then(({resolved}) => {
-    validate(resolved)
+    if (validate) {
+      validate(resolved)
+    }
     return resolved
   })
 }
