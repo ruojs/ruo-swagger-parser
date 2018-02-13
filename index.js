bb79ae06c64b868823f73a2b4d508f151eadefa4
Gaara <sabakugaara@users.noreply.github.com>
2018-02-13 19:18:47 +0800

    fix: validate is not a function

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const fs = require('fs')
-
-const find = require('lodash.find')
 const {resolveRefs: resolve} = require('json-refs')
 
 const parse = require('./lib/parse')
 const validate = require('./lib/validate')
 
-module.exports = function(root, pattern = '**/*.spec.yaml', suffix = '.spec.yaml', prefix = 'api', shadow = false, validate = true) {
+module.exports = function (root, pattern = '**/*.spec.yaml', suffix = '.spec.yaml', prefix = 'api', shadow = false, needValidate = true) {
   const definition = parse(root, pattern, suffix, prefix, shadow)
 
   return resolve(definition).then(({resolved}) => {
-    if (validate) {
+    if (needValidate) {
       validate(resolved)
     }
     return resolved
